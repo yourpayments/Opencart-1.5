ec2c1bf9385ab5f2bd10ec198157bd886ed6227a
PayU Russia <integration@payu.ru>
2014-02-18 14:59:16 +0300

    Update payu.php

--- admin/language/english/payment/payu.php
+++ admin/language/english/payment/payu.php
@@ -4,7 +4,7 @@ $_['heading_title']      = 'PayU';
 
 // Text 
 $_['text_payment']       = 'Payment';
-$_['text_payu']       	 = '<a onclick="window.open(\'http://www.payu.ua/\');"><img src="view/image/payment/payu.png" alt="PayU" title="PayU" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_payu']       	 = '<a onclick="window.open(\'http://www.payu.ru/\');"><img src="view/image/payment/payu.png" alt="PayU" title="PayU" style="border: 1px solid #EEEEEE;" /></a>';
 $_['text_success']       = 'Settings updated';   
 $_['text_pay']           = 'PayU';
 $_['text_card']          = 'Credit Card';
@@ -16,7 +16,7 @@ $_['entry_debug']        = 'Debug mode:';
 $_['entry_debug_on']     = 'On';
 $_['entry_debug_off']    = 'Off';
 
-$_['entry_LU']        	 = 'Link for Live Update:<br /><span class="help">default : for PayU UA </span>';
+$_['entry_LU']        	 = 'Link for Live Update:<br /><span class="help">default : for PayU RU </span>';
 $_['entry_order_status'] = 'Order status after payment:';
 $_['entry_currency']     = 'Currency : ';
 $_['entry_backref']      = 'Back refference :<br /><span class="help">if empty - client will stay at PayU page</span>';

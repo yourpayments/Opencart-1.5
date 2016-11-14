aa386ffa015b39ba958e2fae03a9cc2f6725c03c
Alexandr Kozlov <sanekk1988@mail.ru>
2016-11-14 10:58:56 +0300

    chenge into PayU API class: generate sign for test mode

--- catalog/controller/payment/payu.php
+++ catalog/controller/payment/payu.php
@@ -144,82 +144,166 @@ class ControllerPaymentPayU extends Controller {
 
 class PayU
 {
-	var $luUrl = "https://secure.payu.ru/order/lu.php", 
+	var 
+		$luUrl = "https://secure.payu.ru/order/lu.php", 
 		$button = "<input type='submit'>",
 		$debug = 0,
 		$showinputs = "hidden";
 
-	private static $Inst = false, $merchant, $key;
+	private static $Inst = false, 
+	               $merchant, 
+	               $key;
 
-	private $data = array(), $dataArr = array(), $answer = ""; 
-	private $LUcell = array( 'MERCHANT' => 1, 'ORDER_REF' => 0, 'ORDER_DATE' => 1, 'ORDER_PNAME' => 1, 'ORDER_PGROUP' => 0,
-							'ORDER_PCODE' => 1, 'ORDER_PINFO' => 0, 'ORDER_PRICE' => 1, 'ORDER_QTY' => 1, 'ORDER_VAT' => 1, 
-							'ORDER_SHIPPING' => 1, 'PRICES_CURRENCY' => 1, 'PAY_METHOD' => 0, 'ORDER_PRICE_TYPE' => 1);
+	private $data = array(), 
+	        $dataArr = array(), 
+	        $answer = "";
 
-	private $IPNcell = array( "IPN_PID", "IPN_PNAME", "IPN_DATE", "ORDERSTATUS" );
+	private	$LUcell = array(
+			'MERCHANT' => 1, 
+			'ORDER_REF' => 0, 
+			'ORDER_DATE' => 1, 
+			'ORDER_PNAME' => 1, 
+			'ORDER_PGROUP' => 0,
+			'ORDER_PCODE' => 1, 
+			'ORDER_PINFO' => 0, 
+			'ORDER_PRICE' => 1, 
+			'ORDER_QTY' => 1, 
+			'ORDER_VAT' => 1, 
+			'ORDER_SHIPPING' => 1, 
+			'PRICES_CURRENCY' => 1, 
+			'PAY_METHOD' => 0, 
+			'ORDER_PRICE_TYPE' => 1
+		);
 
-	private function __construct(){}
-	private function __clone(){}
+	private $IPNcell = array(
+		"IPN_PID", 
+		"IPN_PNAME", 
+		"IPN_DATE", 
+		"ORDERSTATUS"
+	);
+
+	private function __construct()
+	{}
+
+	private function __clone()
+	{}
+
 	public function __toString()
 	{ 
-		return ( $this->answer === "" ) ? "<!-- Answer are not exists -->" : $this->answer;  
+		return ($this->answer === "") ? "<!-- Answer are not exists -->" : $this->answer;  
 	}
+
 	public static function getInst()
 	{	
-		if( self::$Inst === false ) self::$Inst = new PayU();
+		if (self::$Inst === false) {
+			self::$Inst = new PayU();
+		}
+
 		return self::$Inst;
 	}
 
-#---------------------------------------------
-# Add all options for PayU object. 
-# Can change all public variables;
-# $opt = array( merchant, secretkey, [ luUrl, debug, button ] );
-#---------------------------------------------
-	function setOptions( $opt = array() )
+	#---------------------------------------------
+	# Add all options for PayU object. 
+	# Can change all public variables;
+	# $opt = array( merchant, secretkey, [ luUrl, debug, button ] );
+	#---------------------------------------------
+	function setOptions($opt = array())
 	{
-		if ( !isset( $opt['merchant'] ) || !isset( $opt['secretkey'] )) die("No params");
+		if (!isset($opt['merchant']) || !isset($opt['secretkey'])) {
+			die("No params");
+		}
+
 		self::$merchant = $opt['merchant'];
 		self::$key = htmlspecialchars_decode( $opt['secretkey'] );
-		unset( $opt['merchant'], $opt['secretkey'] );
-		if ( count($opt) === 0 ) return $this;
-		foreach ( $opt as $k => $v) $this->$k = $v;
+		unset($opt['merchant'], $opt['secretkey']);
+
+		if (count($opt) === 0) {
+			return $this;
+		}
+
+		foreach ($opt as $k => $v) {
+			$this->$k = $v;
+		}
+
 		return $this;
 	}
 
-	function setData( $array = null )
+	function setData($array = null)
 	{	
-		if ($array === null ) die("No data");
+		if ($array === null) {
+			die("No data");
+		}
+
 		$this->dataArr = $array;
+		
 		return $this;
 	}
 
-#--------------------------------------------------------
-#	Generate HASH
-#--------------------------------------------------------
+	#--------------------------------------------------------
+	#	Generate HASH
+	#--------------------------------------------------------
 	function Signature( $data = null ) 
 	{		
 		$str = "";
-		foreach ( $data as $v ) $str .= $this->convData( $v );
-		return hash_hmac("md5",$str, self::$key);
+		foreach ($data as $v) {
+			$str .= $this->convData($v);
+		}
+
+		if ($this->debug > 0) {
+			$str .= '4TRUE';
+		}
+
+		if (function_exists('hash_hmac')) {
+			return hash_hmac("md5", $str, self::$key);
+		}
+
+		return $this->hash_hmac("md5", self::$key,$str);
 	}
 
-#--------------------------------------------------------
-# Outputs a string for hmac format.
-# For a string like 'aa' it will return '2aa'.
-#--------------------------------------------------------
+	function hash_hmac($algo, $data, $key, $raw_output = false)
+	{
+		$algo = strtolower($algo);
+		$pack = 'H'.strlen($algo($key));
+		$size = 64;
+		$opad = str_repeat(chr(0x5C), $size);
+		$ipad = str_repeat(chr(0x36), $size);
+
+		if (strlen($key) > $size) {
+			$key = str_pad(pack($pack, $algo($key)), $size, chr(0x00));
+		} else {
+			$key = str_pad($key, $size, chr(0x00));
+		}
+
+		for ($i = 0; $i < strlen($key) - 1; $i++) {
+			$opad[$i] = $opad[$i] ^ $key[$i];
+			$ipad[$i] = $ipad[$i] ^ $key[$i];
+		}
+
+		$output = $algo($opad.pack($pack, $algo($ipad.$data)));
+
+		return ($raw_output) ? pack($pack, $output) : $output;
+	}
+
+	#--------------------------------------------------------
+	# Outputs a string for hmac format.
+	# For a string like 'aa' it will return '2aa'.
+	#--------------------------------------------------------
 	private function convString($string) 
 	{	
 		return mb_strlen($string, '8bit') . $string;
 	}
 
-#--------------------------------------------------------
-# The same as convString except that it receives
-# an array of strings and returns the string from all values within the array.
-#--------------------------------------------------------	
+	#--------------------------------------------------------
+	# The same as convString except that it receives
+	# an array of strings and returns the string from all values within the array.
+	#--------------------------------------------------------	
 	private function convArray($array) 
 	{
   		$return = '';
-  		foreach ($array as $v) $return .= $this->convString( $v );
+  		foreach ($array as $v) {
+  			$return .= $this->convString( $v );
+  		}
+
   		return $return;
 	}
 
@@ -227,55 +311,72 @@ class PayU
 	{
 		return ( is_array( $val ) ) ? $this->convArray( $val ) : $this->convString( $val );
 	}
-#----------------------------
+	#----------------------------
 
-#====================== LU GENERETE FORM =================================================
+	#====================== LU GENERETE FORM =================================================
 
 	public function LU()
 	{	
 		$arr = &$this->dataArr;
 		$arr['MERCHANT'] = self::$merchant;
-		if( !isset($arr['ORDER_DATE']) ) $arr['ORDER_DATE'] = date("Y-m-d H:i:s");
-		$arr['TESTORDER'] = ( $this->debug == 1 ) ? "TRUE" : "FALSE";
+
+		if (!isset($arr['ORDER_DATE'])) {
+			$arr['ORDER_DATE'] = date("Y-m-d H:i:s");
+		}
+
+		$arr['TESTORDER']  = ($this->debug == 1) ? "TRUE" : "FALSE";
 		$arr['DEBUG'] = $this->debug;
-		$arr['ORDER_HASH'] = $this->Signature( $this->checkArray( $arr ) );
-		$this->answer = $this->genereteForm( $arr );
+
+		$arr['ORDER_HASH'] = $this->Signature($this->checkArray($arr));
+		$this->answer = $this->genereteForm($arr);
 		return $this;
 	}
 
-#-----------------------------
-# Check array for correct data
-#-----------------------------
+	#-----------------------------
+	# Check array for correct data
+	#-----------------------------
 	private function checkArray( $data )
 	{
 		$this->cells = array();
 		$ret = array();
-		foreach ( $this->LUcell as $k => $v ) 
-		{ 	
-			if ( isset($data[$k]) ) $ret[$k] = $data[$k];
-			 elseif ( $v == 1 ) die("$k is not set");
+		foreach ($this->LUcell as $k => $v) { 	
+			if (isset($data[$k])) {
+				$ret[$k] = $data[$k];
+			} elseif ($v == 1) {
+				die("$k is not set");
+			}
 		}
+
 		return $ret;
 	}
 
-#-----------------------------
-# Method which create a form
-#-----------------------------
-	private function genereteForm( $data )
+	#-----------------------------
+	# Method which create a form
+	#-----------------------------
+	private function genereteForm($data)
 	{	
-		$form = '<form method="post" action="'.$this->luUrl.'" accept-charset="utf-8">';
-		foreach ( $data as $k => $v ) $form .= $this->makeString( $k, $v );
-		return $form . $this->button."</form>";
+		$form = '<form target="_blank" method="post" action="'.$this->luUrl.'" accept-charset="utf-8">';
+		foreach ($data as $k => $v) {
+			$form .= $this->makeString( $k, $v );
+		}
+
+		return $form . $this->button .'</form>';
 	}	
 
-#-----------------------------
-# Make inputs for form
-#-----------------------------	
-	private function makeString ( $name, $val )
+	#-----------------------------
+	# Make inputs for form
+	#-----------------------------	
+	private function makeString($name, $val)
 	{
 		$str = "";
-		if ( !is_array( $val ) ) return '<input type="'.$this->showinputs.'" name="'.$name.'" value="'.htmlspecialchars($val).'">'."\n";
-		foreach ($val as $v) $str .= $this->makeString( $name.'[]', $v );
+		if (!is_array($val)) {
+			return '<input type="'.$this->showinputs.'" name="'.$name.'" value="'.htmlspecialchars($val).'">'."\n";
+		}
+
+		foreach ($val as $v) {
+			$str .= $this->makeString( $name.'[]', $v );
+		}
+
 		return $str;
 	}
 
@@ -288,49 +389,60 @@ class PayU
 	{	
 		$arr = &$this->dataArr;
 		$arr = $_POST;
-		foreach ( $this->IPNcell as $name ) if ( !isset( $arr[ $name ] ) ) die( "Incorrect data" );
+
+		foreach ($this->IPNcell as $name) {
+			if (!isset($arr[$name])) {
+				die("Incorrect data");
+			}
+		}
 
 		$hash = $arr["HASH"];  
-		unset( $arr["HASH"] );
-		$sign = $this->Signature( $arr );
+		unset($arr["HASH"]);
+		$sign = $this->Signature($arr);
 
-		if ( $hash != $sign ) return $this;
+		if ($hash != $sign) {
+			return $this;
+		}
+
 		$datetime = date("YmdHis");
-		$sign = $this->Signature(  array(
-				   						"IPN_PID" => $arr[ "IPN_PID" ][0], 
-				  						"IPN_PNAME" => $arr[ "IPN_PNAME" ][0], 
-				   						"IPN_DATE" => $arr[ "IPN_DATE" ], 
-				   						"DATE" => $datetime
-										)
-								);
+		$sign = $this->Signature(array(
+			"IPN_PID" => $arr[ "IPN_PID" ][0], 
+			"IPN_PNAME" => $arr[ "IPN_PNAME" ][0], 
+			"IPN_DATE" => $arr[ "IPN_DATE" ], 
+			"DATE" => $datetime
+		));
 
 		$this->answer = "<!-- <EPAYMENT>$datetime|$sign</EPAYMENT> -->";
+
 		return $this;
 	}
 
-#======================= END IPN ============================
+	#======================= END IPN ============================
 
-#======================= Check BACK_REF =====================
+	#======================= Check BACK_REF =====================
 	function checkBackRef( $type = "http")
 	{
 		$path = $type.'://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
 		$tmp = explode("?", $path);
 		$url = $tmp[0].'?';
 		$params = array();
-		foreach ($_GET as $k => $v)
-		{
-			if ( $k != "ctrl" ) $params[] = $k.'='.rawurlencode($v);
+
+		foreach ($_GET as $k => $v) {
+			if ($k != "ctrl") {
+				$params[] = $k.'='.rawurlencode($v);
+			}
 		}
+
 		$url = $url.implode("&", $params);
 		$arr = array($url);
 		$sign = $this->Signature( $arr );
 
 		#echo "$sign === ".$_GET['ctrl'];
 		$this->answer = ( $sign === $_GET['ctrl'] ) ? true : false;
+
 		return $this->answer;
 	}
 
-#======================= END Check BACK_REF =================
-
+	#======================= END Check BACK_REF =================
 }
 ?>
